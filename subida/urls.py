8accea6256d586388872bfa0849046efd9bf684b
Jonathan Lara <jonabimael@gmail.com>
2020-08-07 11:41:02 -0500

    .

--- subida/urls.py
+++ subida/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
+app_name = 'Subida'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('modelos/', views.VistaSubidaModelos.as_view(), name='modelos')
