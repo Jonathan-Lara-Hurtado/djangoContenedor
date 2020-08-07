a367cc7a8be6eac05d3b7a0281762e9c2c0e7a1f
Jonathan Lara <jonabimael@gmail.com>
2020-08-06 22:32:07 -0500

    .

--- pagina/settings.py
+++ pagina/settings.py
@@ -12,12 +12,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from socket import gethostname
-OPENSHIFT_DATA_DIR = '/opt/app-root/src/data'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DATA_DIR = os.environ.get(OPENSHIFT_DATA_DIR, BASE_DIR)
+DATA_DIR = os.environ.get(os.environ['OPENSHIFT_DATA_DIR'], BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -79,7 +78,7 @@ WSGI_APPLICATION = 'pagina.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(OPENSHIFT_DATA_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(DATA_DIR, 'db.sqlite3'),
     }
 }
 
@@ -133,5 +132,5 @@ STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = [
-os.path.join(OPENSHIFT_DATA_DIR,"static"),
+os.path.join(DATA_DIR,"static"),
 ]
