ae88999cb70282af71b746b6f56e54b05af476c6
Jonathan Lara <jonabimael@gmail.com>
2020-08-06 21:14:28 -0500

    .

--- pagina/settings.py
+++ pagina/settings.py
@@ -12,10 +12,12 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from socket import gethostname
+OPENSHIFT_DATA_DIR = '/opt/app-root/src/data'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+DATA_DIR = os.environ.get(OPENSHIFT_DATA_DIR, BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -123,3 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+os.path.join(DATA_DIR,"static"),
+]
