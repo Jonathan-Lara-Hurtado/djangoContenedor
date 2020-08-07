1e17d840d96fe9613d171b5c0dbfdfaba542cd9a
Jonathan Lara <jonabimael@gmail.com>
2020-08-06 22:55:28 -0500

    .

--- pagina/settings.py
+++ pagina/settings.py
@@ -16,7 +16,7 @@ from socket import gethostname
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DATA_DIR = os.environ.get(os.environ['OPENSHIFTDATADIR'], BASE_DIR)
+#DATA_DIR = os.environ.get(os.environ['OPENSHIFTDATADIR'], BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
