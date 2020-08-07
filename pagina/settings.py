29d3d07fd01b0dd587df171471624f4a7b6478d4
Jonathan Lara <jonabimael@gmail.com>
2020-08-06 21:19:35 -0500

    .

--- pagina/settings.py
+++ pagina/settings.py
@@ -78,15 +78,20 @@ WSGI_APPLICATION = 'pagina.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+"""
+    'ENGINE': 'django.db.backends.mysql',
         'NAME': 'pruebas',
         'USER':'admin',
         'PASSWORD':'12345',
         'HOST':'pruebasmysql.gedeon.svc',
         'PORT':'3306',
-    }
-}
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
