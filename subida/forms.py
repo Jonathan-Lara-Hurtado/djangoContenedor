df27f5da5f237d365bf3ecb46710fc312c6fa26f
Jonathan Lara <jonabimael@gmail.com>
2020-08-06 23:59:48 -0500

    .

---
+++ subida/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.forms import ModelForm
+from .models import Producto
+
+
+class UploadFileForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    archivo = forms.FileField()
+
+
+class FormSubida(ModelForm):
+    class Meta:
+        model = Producto
+        fields = ['titulo','descripcion','imagen']
