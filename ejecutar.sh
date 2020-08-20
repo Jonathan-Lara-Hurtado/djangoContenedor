da88a922791e82b74b3d04e18776c18a9166b8b8
Jonathan Lara <jonabimael@gmail.com>
2020-08-19 20:31:28 -0500

    Reconfigurando Dokerfile

--- ejecutar.sh
+++ ejecutar.sh
@@ -2,9 +2,9 @@
 
 ARGS=""
 ARGS="$ARGS --port 8080"
-ARGS="$ARGS --url-alias /static ArchivosEstaticos/"
-ARGS="$ARGS --url-alias /ArchivosMedia ArchivosMedia/"
-ARGS="$ARGS --url-alias /favicon.ico ArchivosEstaticos/"
+#ARGS="$ARGS --url-alias /static ArchivosEstaticos/"
+#ARGS="$ARGS --url-alias /ArchivosMedia ArchivosMedia/"
+#ARGS="$ARGS --url-alias /favicon.ico ArchivosEstaticos/"
 
 exec mod_wsgi-express start-server $ARGS pagina/wsgi.py
 
