a29158b8d65dfd6dc55122f21d9448b89b8b1ad2
Jonathan Lara <jonabimael@gmail.com>
2020-08-20 10:35:03 -0500

    Reconfigurando Dokerfile

--- ejecutar.sh
+++ ejecutar.sh
@@ -1,31 +1,21 @@
 #!/bin/bash
-
-#echo "Iniciando proceso"
-
-echo "migrations"
 python manage.py makemigrations
-
-echo "migrate"
 python manage.py migrate
-
 python manage.py collectstatic --noinput
 
+REDHATOPENSHIFT=false
 ARGS=""
 ARGS="$ARGS --log-to-terminal"
 ARGS="$ARGS --port 8080"
-ARGS="$ARGS --url-alias /static $OPENSHIFTDATADIR/ArchivosEstaticos/"
-ARGS="$ARGS --url-alias /ArchivosMedia $OPENSHIFTDATADIR/ArchivosMedia/"
-ARGS="$ARGS --url-alias /favicon.ico $OPENSHIFTDATADIR/ArchivosEstaticos/"
+if $REDHATOPENSHIFT
+then
+  ARGS="$ARGS --url-alias /static $OPENSHIFTDATADIR/ArchivosEstaticos/"
+  ARGS="$ARGS --url-alias /ArchivosMedia $OPENSHIFTDATADIR/ArchivosMedia/"
+  ARGS="$ARGS --url-alias /favicon.ico $OPENSHIFTDATADIR/ArchivosEstaticos/"
 #ARGS="$ARGS --httpd-executable /usr/sbin/apache2"
+else
+  ARGS="$ARGS --url-alias /static ArchivosEstaticos/"
+  ARGS="$ARGS --url-alias /ArchivosMedia ArchivosMedia/"
+  ARGS="$ARGS --url-alias /favicon.ico ArchivosEstaticos/"
+fi
 exec mod_wsgi-express start-server $ARGS pagina/wsgi.py
-
-#echo "Iniciando proceso"
-
-#echo "migrations"
-#python manage.py makemigrations
-
-#echo "migrate"
-#python manage.py migrate
-
-#echo "runserver"
-#python manage.py runserver 0.0.0.0:8000
