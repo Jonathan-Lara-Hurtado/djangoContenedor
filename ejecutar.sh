5efaee6a51b1ec0ed4701f21370e1cba079e9747
Jonathan Lara <jonabimael@gmail.com>
2020-09-01 00:10:55 -0500

    cambio

--- ejecutar.sh
+++ ejecutar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python manage.py makemigrations
-python manage.py migrate
+#python manage.py makemigrations
+#python manage.py migrate
 python manage.py collectstatic --noinput -c
 python manage.py collectstatic --noinput
 
