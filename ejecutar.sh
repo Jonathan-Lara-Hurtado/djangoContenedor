b9d82c2a7c8cb038640cd50e61af8347675a4cba
Jonathan Lara <jonabimael@gmail.com>
2020-08-31 23:27:30 -0500

    Path

--- ejecutar.sh
+++ ejecutar.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-chmod 777 almacenamiento
 
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py collectstatic --noinput -c
+#python3 manage.py collectstatic --noinput -c
 python3 manage.py collectstatic --noinput
 
 REDHATOPENSHIFT=true
